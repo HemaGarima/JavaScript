20bc5ef6b5ba2b6bf6a10b92dfdb2a1369e0e9b5
Hema <hemlatasharma23052003@gmail.com>
2024-01-13 21:58:33 +0530

    operators in js

---
+++ 06_operators.js
@@ -0,0 +1,36 @@
+// This is a tutorial from codewithharry
+// console.log("Operators in JS");
+let a = 10;
+let b = 4;
+
+// ARITHMETIC OPERATORS
+// console.log("a + b =", a+b);
+// console.log("a - b =", a-b);
+// console.log("a / b =", a/b);
+// console.log("a ** b =", a**b);
+// console.log("a % b =", a%b);
+// console.log("++a =", ++a);
+// console.log("a =", a);
+// console.log("a-- =", a--);
+
+// ASSIGNMENT OPERATORS
+a += 5;
+console.log(a);
+
+// COMPARISON OPERATORS
+let comp1 = 6;
+let comp2 = "6";
+console.log("comp1 == comp2 is", comp1 == comp2);
+console.log("comp1 != comp2 is", comp1 != comp2);
+console.log("comp1 === comp2 is", comp1 === comp2); // equal value and type
+console.log("comp1 !== comp2 is", comp1 !== comp2);
+console.log("comp1 > comp2 is", comp1 > comp2);
+
+// LOGICAL OPERATORS
+let x = 5;
+let y = 6;
+console.log(x<y && x == 5);
+console.log(x>y || x == 5);
+
+// NOT OPERATORS
+console.log(!false);
