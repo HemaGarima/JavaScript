4e58ea86350e52634661ed97525f3b05ddcd66b9
Hema <hemlatasharma23052003@gmail.com>
2024-01-13 22:01:03 +0530

    for Loops

---
+++ 09_for_loops.js
@@ -0,0 +1,26 @@
+const prompt = require("prompt-sync")();
+// Program to add first n natural numbers
+let sum = 0;
+let n = prompt("Enter the value of n.");
+n = Number.parseInt(n);
+for(let i = 0;i<n;i++){
+    sum += (i+1);
+}
+console.log("Sum of first " + n + " natural numbers is " + sum);
+
+let obj = {
+    harry : 90,
+    shubh : 45,
+    shivika : 56,
+    ritika : 57,
+    shiv : 23
+}
+// For in loop
+for(let a in obj){
+    console.log("Marks of " + a + " are " + obj[a]);
+}
+// For of loop
+for(let b of "Harry"){
+    console.log(b);
+}
+// console.log(typeof(harry));
