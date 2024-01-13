ccc4992f8b258082c937456864152330debd2124
Hema <hemlatasharma23052003@gmail.com>
2024-01-13 21:54:47 +0530

    intro to variables

---
+++ 02_variables.js
@@ -0,0 +1,11 @@
+console.log("Hello world.");
+let a = 67; // a contains 67.
+console.log(a);
+a = "harry";
+console.log(a);
+// let 8hema = 8; // Not allowed , this will throw an error.
+// let var = 4; // this will throw an error, reserve keyword not allowed
+let h = 7;
+let H = 8;
+console.log(h);
+console.log(H);
