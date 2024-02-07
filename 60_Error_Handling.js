d0d7ff5cd35301f4633e155408259c6a8a0ebfeb
Hema <hemlatasharma23052003@gmail.com>
2024-02-07 21:43:10 +0530

    Error handling - Try and catch

---
+++ 60_Error_Handling.js
@@ -0,0 +1,25 @@
+setTimeout(()=>{
+    console.log("Hacking wifi..... Please wait.... ");
+},1000)
+try{
+    console.log(rahul);// this is the error
+    // setTimeout(()=>{
+    //     console.log(rahul);
+    // },100) // try can't able to catch this
+}
+catch(error){
+    // console.log(error);
+    console.log("Balle Balle!!!!!!" + error);
+}
+setTimeout(()=>{
+    console.log("Fetching username and password..... and Please wait.... ");
+},2000)
+
+setTimeout(()=>{
+    console.log("Hacking Rahul's facebook id..... Please wait.... ");
+    
+},3000)
+
+setTimeout(()=>{
+    console.log("Username and password of Rahul (+915834059938) fetched!!!!!");
+},4000)
